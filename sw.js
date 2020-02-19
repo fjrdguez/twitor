8e4039dcf9be8e95ec2d39d0ca3327a0b7851dc9
Fco. javier rodríguez <fjrdguez@gmail.com>
2020-02-19 18:01:41 +0100

    asdfa

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const STATIC_CACHE = "static-v2";
 const DINAMIC_CACHE = "dinamic-v1";
 const INMUTABLE_CACHE = "inmutable-v1";
 const APP_SHELL = [
-    '/',
+    //'/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
